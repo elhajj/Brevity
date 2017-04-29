00ca7bb92ff0ade55a8e19c53272e4849dfa9849
ElhajjT <tim@telhajj.com>
2017-04-28 17:03:06 -0700

    more work on this

--- writeXML.py
+++ writeXML.py
@@ -1,4 +1,20 @@
 import xml.etree.ElementTree as ET
 
 rss = ET.Element("rss")
-channel = ET.Element(rss, "channel")
+channel = ET.SubElement(rss, "channel")
+
+#ET.SubElement(doc, "field1", name="blah").text = "some value1"
+#ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+
+#headers
+'''
+<rss 
+xmlns:excerpt="http://wordpress.org/export/1.2/excerpt/" 
+xmlns:content="http://purl.org/rss/1.0/modules/content/" 
+xmlns:wfw="http://wellformedweb.org/CommentAPI/" 
+xmlns:dc="http://purl.org/dc/elements/1.1/" 
+xmlns:wp="http://wordpress.org/export/1.2/" version="2.0">
+
+'''
+tree = ET.ElementTree(rss)
+tree.write("filename.xml")
