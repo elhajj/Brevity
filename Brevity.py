55d1848344b8eef2a62d841534c40df0eb73e337
elhajj <tim@telhajj.com>
2017-05-10 23:33:29 -0700

    better title and story selection, clean up story (remove title, add fully qualified url to image, remove author name, remove hr and insert two underscores)

--- Brevity.py
+++ Brevity.py
@@ -11,22 +11,28 @@ print 'Retrieved',len(data),'characters'
 
 soup = BeautifulSoup(data, 'html.parser')
 
-title = soup.body.center.table.tr.next_sibling.next_sibling.div.text.lstrip()
+title = soup.find_all('div', 'storytitle')
+title = title[0].text.lstrip()
+
 author = soup.select('strong > a')
 author = list(author)[0].text
-soup.body.center.table.tr.next_sibling.next_sibling.div.decompose() # removes title
-#soup.body.center.table.tr.next_sibling.next_sibling.td.decompose() # removes left margin thingy
-#soup.body.center.table.tr.next_sibling.next_sibling.td.unwrap() # remove td around story
+
+cells = soup.find_all('td')
+story = cells[len(cells)-1]
+story = story.find_all('p')
+
+shrtaddress = address.rstrip(author+'_'+title+'.html')
+addy_append = story[0].find('img').get('src')
+story[0].find('img')['src'] = shrtaddress+addy_append
+story[0].find('strong').decompose()
+new_tag = soup.new_tag('p')
+new_tag.string = '__'
+story.insert(len(story)-3,new_tag)
 
 
 print 'This is the title:', title
 print 'Author name:', author
 
-#story = soup.body.center.table.tr.next_sibling.next_sibling.td.next_sibling.next_sibling
-#list(story.children)[3].strong.decompose() # removes the author attribution at the top of story
-#list(story.children)[1].br.decompose() # removes the title at the top of the story
-#story = unicode(story)
-#print story
 
 
 
