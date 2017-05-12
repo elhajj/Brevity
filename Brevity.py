0a3661b1b856648bc91cbeeb5e76dbdac625797c
elhajj <tim@telhajj.com>
2017-05-12 01:01:18 -0700

    get the author name from title code for image url

--- Brevity.py
+++ Brevity.py
@@ -3,7 +3,11 @@ import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 import re
 
-address = 'https://www.creativenonfiction.org/brevity/past%20issues/brev28/calderazzo_accident.html'
+#address = 'https://www.creativenonfiction.org/brevity/past%20issues/brev28/calderazzo_accident.html'
+#address = 'https://www.creativenonfiction.org/brevity/past%20issues/brev28/madden_catch.html'
+#address = 'https://www.creativenonfiction.org/brevity/past%20issues/brev28/svoboda_catholic.html'
+address = 'https://www.creativenonfiction.org/brevity/past%20issues/brev28/doyle_child.html'
+
 uh = urllib.urlopen(address)
 print 'Retrieving', address
 data = uh.read()
@@ -14,27 +18,36 @@ soup = BeautifulSoup(data, 'html.parser')
 title = soup.find_all('div', 'storytitle')
 title = title[0].text.lstrip()
 
-author = soup.select('strong > a')
-author = list(author)[0].text
-author = author.upper()
+author = soup.title.contents
+author = str(author)
+author = author[15:-2].upper() # this will break < issue 10
 
-cells = soup.find_all('td')
-story = cells[len(cells)-1]
-story = story.find_all('p')
+story = soup.find_all('p')
 
-shrtaddress = address.rstrip(author+'_'+title+'.html')
-addy_append = story[0].find('img').get('src')
-story[0].find('img')['src'] = shrtaddress+addy_append
-story[0].find('strong').decompose()
-new_tag = soup.new_tag('p')
-new_tag.string = '__'
-story.insert(len(story)-3,new_tag)
+shrtaddress = address[:64-len(address)] # this will break < issue 10
+image = soup.find_all('img')
+if len(soup.find_all('img')) != 2:
+    print "image is probably broken for", title
+image = image[1]
+addy_append = image.get('src')
+image['src'] = shrtaddress+addy_append
+
+
+#story[0].find('strong').decompose()
+#new_tag = soup.new_tag('p')
+#new_tag.string = '__'
+#story.insert(len(story)-3,new_tag)
+
 
 
 print 'This is the title:', title
 print 'Author name:', author
 
+for p in story:
+    print p.text
 
+for p in story:
+    print p
 
 
 
